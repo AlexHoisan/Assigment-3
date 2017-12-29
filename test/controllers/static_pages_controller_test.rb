51cbeacce4f4cc2ce5486af1781ee329097ecc9f
Hoisan Alex <hoisan_alex@yahoo.com>
2017-12-29 19:49:21 +0100

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,29 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', 'Food Show'
   end
 
-  test "should get about" do
+  test 'should get about' do
     get static_pages_about_url
     assert_response :success
   end
 
-  test "should get service" do
+  test 'should get service' do
     get static_pages_service_url
     assert_response :success
   end
 
-  test "should get blog" do
+  test 'should get blog' do
     get static_pages_blog_url
     assert_response :success
   end
 
-  test "should get contacts" do
+  test 'should get contacts' do
     get static_pages_contacts_url
     assert_response :success
   end
-
 end
