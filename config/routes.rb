82e0d5c70f4b7fbc8c48a079edf8d9ac972e3612
Hoisan Alex <hoisan_alex@yahoo.com>
2018-01-25 18:20:09 +0100

    Generate Blog and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  resources :blogs
   get '/about',      to: 'static_pages#about'
   get '/service',    to: 'static_pages#service'
-  get '/blog',       to: 'static_pages#blog'
+  get '/blog',       to: 'blogs#index'
   get '/contacts',   to: 'static_pages#contacts'
 
   root 'static_pages#home'
